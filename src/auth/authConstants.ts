42be7a619fafe6502e12c2e03c9cf161fb4c25fa
Jorge C <jorgecolmenarez89@gmail.com>
2023-11-01 09:35:24 -0400

    change api url

--- src/auth/authConstants.ts
+++ src/auth/authConstants.ts
@@ -1,2 +1,2 @@
 //export const API_URL = "http://localhost:3100/api";
-export const API_URL =  process.env.NODE_ENV === 'development' ? "http://localhost:3100/api" : "https://backend-evaluaci-n-de-salud-y-bienestar-para-rrhh-mji4y2ty2.vercel.app/api";
+export const API_URL =  process.env.NODE_ENV === 'development' ? "http://localhost:3100/api" : "https://backend-evaluaci-n-de-salud-y-bienestar-para-rrhh-beryl.vercel.app/api";
