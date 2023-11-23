663309c6ca1d703b3877ecb03c50bdfbe5de369f
Jorge C <jorgecolmenarez89@gmail.com>
2023-11-23 19:30:03 -0400

    remove contante sin uso

--- src/routes/Cuestionarios.tsx
+++ src/routes/Cuestionarios.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import PortalLayout from "../layout/PortalLayout";
 import { API_URL } from "../auth/authConstants";
 import Card from "react-bootstrap/Card";
